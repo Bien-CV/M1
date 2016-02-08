4e1e5b9bb0508f3a9baf69b2674fe648f5bc80b7
dennisman <dennisman@laposte.net>
2016-02-08 15:11:27 +0100

    debut musique proj

--- multicore programing/tp/tp1/search-seq-2cores.cpp
+++ multicore programing/tp/tp1/search-seq-2cores.cpp
@@ -8,18 +8,36 @@
 using namespace std;
 using namespace tbb;
 
+//// g++ −Wall −o para_for para_for.cpp −ltbb
 typedef blocked_range<int> range;
 //-----------------class searchSeq-------------------
 class searchSeq{
 public:
-	searchSeq(vector<string>& a), double v): T(a), val(v){}
+	searchSeq(vector<string>& a), int v): T(a), len(v){}
 	void operator()(const range& r) const{
-		for (int i=r.begin(); i!=r.end();++i){
-			// coeur de la fct
+		for(string line : dico)
+		{
+			if(line.size()==len){
+				cpt = 0;
+				for(i = 0; i<len;++i){
+					if(  chaine[i]=='.' or line[i] ==chaine[i]){
+						cpt++;
+					}else{
+						break;
+					}
+				}
+				if(cpt==len){
+					cout<<line<<endl;
+				}
+			}
+	  		
 		}
 	}
 private:
-}
+	vector<string>& T;
+	int len;
+	
+};
 int main(int argc, char** argv){
 	if(argc!=2){ // argument 0 = nom du prog, argument 1 = premier argument
 		cout << "vous devez passez le mot à trouver en parametre" << endl;
@@ -43,32 +61,16 @@ int main(int argc, char** argv){
     	}
     sowpods.close();
   	}
+  	
+  	const int sz = 267751;
     
-    
-	
 	//compteur
     auto begin = std::chrono::high_resolution_clock::now();
     	
 	int len= chaine.size();
 
-	for(string line : dico)
-	{
-		if(line.size()==len){
-			cpt = 0;
-			for(i = 0; i<len;++i){
-				if(  chaine[i]=='.' or line[i] ==chaine[i]){
-					cpt++;
-				}else{
-					break;
-				}
-			}
-			if(cpt==len){
-				cout<<line<<endl;
-			}
-		}
-  		
-	}
-   		
+	
+   	parallel_for(range(0,sz,26751/2), searchSeq(dico, len));	
 	
 	
 	
@@ -78,7 +80,9 @@ int main(int argc, char** argv){
 	
 	auto end = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count() << "ms" << std::endl;
+
    
+//---------------------nouveau main-----------------------
 
 	return 1;
 }
